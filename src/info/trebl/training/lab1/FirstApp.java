5729372e925e36713527d6e20536f52c9f8b4347
Nazariy777 <nazarko.fedoriv@ukr.net>
2015-09-15 12:31:45 +0300

    створив новий проект

---
+++ src/info/trebl/training/lab1/FirstApp.java
@@ -0,0 +1,9 @@
+package info.trebl.training.lab1;
+
+/**
+ * Created by kibernetuk.com on 15.09.2015.
+ */
+public class FirstApp {
+    public static void main(String[] args){
+System.out.println (1+2+3+4+5);}
+    }
