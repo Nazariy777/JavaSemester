9423258ef105462627fb51d0eae6440bf1774410
Nazariy777 <nazarko.fedoriv@ukr.net>
2015-09-15 12:33:23 +0300

    виправив

--- src/info/trebl/training/lab1/FirstApp.java
+++ src/info/trebl/training/lab1/FirstApp.java
@@ -1,8 +1,5 @@
 package info.trebl.training.lab1;
 
-/**
- * Created by kibernetuk.com on 15.09.2015.
- */
 public class FirstApp {
     public static void main(String[] args){
 System.out.println (1+2+3+4+5);}
