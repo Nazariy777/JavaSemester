7ebd9848c92b425363cf89f4d02347dd9ab7ab61
Nazariy777 <nazarko.fedoriv@ukr.net>
2015-10-26 17:39:14 +0200

    додав

---
+++ src/info/trebl/training/lab1/LiniuneRivnannia/LinearEquations.java
@@ -0,0 +1,15 @@
+package info.trebl.training.lab1.LiniuneRivnannia;
+
+import java.util.Scanner;
+
+/**
+ * Created by kibernetuk.com on 26.10.2015.
+ */
+public class LinearEquations{
+    public static void main(String[] args){
+        double a = 4;
+        double b = 12;
+        double x = -b / a;
+        System.out.println(x);
+    }
+}
